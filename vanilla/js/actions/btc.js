738d78869a2a77113e52ffbeeb7714f51500e2ef
BuildTools <unconfigured@null.spigotmc.org>
2018-08-02 22:09:22 +0400

    etc+btc actions
    btc.send and eth.send not tested

--- vanilla/js/actions/btc.js
+++ vanilla/js/actions/btc.js
@@ -1,4 +1,7 @@
 APP.Actions.btc = {
+	login : function (privateKey, callback) {
+		/* todo */
+	},
 	getBalance : function (callback) {
 		const address = APP.CORE.services.auth.accounts.btc.getAddress();
 		const url = `${config.api.bitpay}/addr/${address}`;
@@ -8,6 +11,182 @@ APP.Actions.btc = {
 			complete : function (rv) {
 				if (callback instanceof Function) {
 					callback(rv.responseJSON.balance);
+				}
+			}
+		} );
+	},
+	fetchBalance : function (address,callback) {
+		$.ajax( {
+			type : 'GET',
+			url : `${config.api.bitpay}/addr/${address}`,
+			complete : function (rv) {
+				if (callback instanceof Function) {
+					callback(
+						rv.responseJSON.balance,
+						rv.responseJSON.unconfirmedBalance
+					)
+				}
+			}
+		} );
+	},
+	getTransaction : function (callback) {
+		new Promise((resolve) => {
+			const address = APP.CORE.services.auth.accounts.btc.getAddress();
+			
+			const url = `${config.api.bitpay}/txs/?address=${address}`
+			
+			let transactions;
+			
+			$.ajax( {
+				type : 'GET',
+				url : url,
+				complete : function (rv) {
+					if (rv.responseJSON) {
+						const res = rv.responseJSON;
+						transactions = res.txs.map((item) => ({
+							type: 'btc',
+							hash: item.txid,
+							confirmations: item.confirmations,
+							value: item.vout[0].value,
+							date: item.time * 1000,
+							direction: address.toLocaleLowerCase() === item.vout[0].scriptPubKey.addresses[0].toLocaleLowerCase() ? 'in' : 'out',
+						}));
+						if (callback instanceof Function) {
+							callback(transactions);
+						}
+					} else {
+						if (callback instanceof Function) {
+							callback([]);
+							console.error('res:status BTC false', res)
+						}
+					}
+				}
+			});
+		})
+	},
+	createScript : function (data) {
+		const { secretHash, ownerPublicKey, recipientPublicKey, lockTime } = data
+		let bitcoin = APP.CORE.env.bitcoin;
+		const network = (
+			APP.CORE.isMainNet
+			? bitcoin.networks.bitcoin
+			: bitcoin.networks.testnet
+		);
+
+		const script = bitcoin.script.compile([
+
+			bitcoin.opcodes.OP_RIPEMD160,
+			Buffer.from(secretHash, 'hex'),
+			bitcoin.opcodes.OP_EQUALVERIFY,
+
+			Buffer.from(recipientPublicKey, 'hex'),
+			bitcoin.opcodes.OP_EQUAL,
+			bitcoin.opcodes.OP_IF,
+
+			Buffer.from(recipientPublicKey, 'hex'),
+			bitcoin.opcodes.OP_CHECKSIG,
+
+			bitcoin.opcodes.OP_ELSE,
+
+			bitcoin.script.number.encode(lockTime),
+			bitcoin.opcodes.OP_CHECKLOCKTIMEVERIFY,
+			bitcoin.opcodes.OP_DROP,
+			Buffer.from(ownerPublicKey, 'hex'),
+			bitcoin.opcodes.OP_CHECKSIG,
+
+			bitcoin.opcodes.OP_ENDIF,
+		])
+
+		const scriptPubKey  = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(script))
+		const scriptAddress = bitcoin.address.fromOutputScript(scriptPubKey, network)
+
+		return {
+			scriptAddress,
+		}
+	},
+	send : function (from, to, amount, callback ) {
+		const privateKey = APP.CORE.services.auth.accounts.btc.getPrivateKey();
+		let bitcoin = APP.CORE.env.bitcoin;
+		const newtx = {
+			inputs: [
+				{
+					addresses: [ from ],
+				},
+			],
+			outputs: [
+				{
+					addresses: [ to ],
+					value: amount * 100000000,
+				},
+			],
+		};
+		console.log(1,newtx);
+		$.ajax({
+			type: "POST",
+			url: 'https://api.blockcypher.com/v1/btc/test3/txs/new',
+			data: JSON.stringify(newtx),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			complete : function (rv) {
+				let d = rv.responseJSON;
+				console.log(d);
+				const tmptx = {
+					...d,
+					pubkeys: [],
+				};
+				const network = (
+					APP.CORE.isMainNet
+					? bitcoin.networks.bitcoin
+					: bitcoin.networks.testnet
+				);
+				console.log(privateKey,network);
+				const keys = new bitcoin.ECPair.fromWIF(
+					privateKey, 
+					network
+				); // eslint-disable-line
+
+				tmptx.signatures = tmptx.tosign.map((toSign) => {
+					tmptx.pubkeys.push(keys.getPublicKeyBuffer().toString('hex'))
+					return keys.sign(BigInteger.fromHex(toSign.toString('hex')).toBuffer()).toDER().toString('hex')
+				});
+				console.log(tmptx);
+				$.ajax( {
+					type : 'POST',
+					url : 'https://api.blockcypher.com/v1/btc/test3/txs/send',
+					data: JSON.stringify(tmptx),
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					complete : function (rv) {
+						const res = rv.responseJSON;
+						if (callback instanceof Function) {
+							callback(tmptx);
+						}
+					}
+				} );
+			} 
+		});
+	},
+	fetchUnspents : function (address, callback) {
+		$.ajax( {
+			type : 'GET',
+			url : `${config.api.bitpay}/addr/${address}/utxo`,
+			complete : function (rv) {
+				if (callback instanceof Function) {
+					callback(rv.responseJSON);
+				}
+			}
+		} );
+	},
+	broadcastTx : function (txRaw, callback) {
+		$.ajax( {
+			type : 'POST',
+			url : `${config.api.bitpay}/tx/send`,
+			data: JSON.stringify({
+				rawtx: txRaw,
+			} ),
+			complete : function (rv) {
+				if (callback instanceof Function) {
+					callback(rv.responseJSON);
 				}
 			}
 		} );
--- vanilla/js/actions/eth.js
+++ vanilla/js/actions/eth.js
@@ -1,4 +1,7 @@
 APP.Actions.eth = {
+	login : function (privateKey, callback ) {
+		/* todo */
+	},
 	getBalance : function (callback) {
 		APP.CORE.env.web3.eth.getBalance(
 			APP.CORE.services.auth.accounts.eth.address
@@ -13,5 +16,88 @@ APP.Actions.eth = {
 		.catch((e) => {
 			console.log('Web3 doesn\'t work please again later ',  e.error)
 		});
+	},
+	fetchBalance : function (address, callback) {
+		APP.CORE.env.web3.eth.getBalance(address)
+			.then(result => {
+				if (callback instanceof Function) {
+					callback(APP.CORE.env.web3.utils.fromWei(result))
+				}
+				return Number(APP.CORE.env.web3.utils.fromWei(result))
+			})
+			.catch((e) => {
+				console.log('Web3 doesn\'t work please again later ', e.error)
+			});
+	},
+	getTransaction : function (callback) {
+		new Promise((resolve) => {
+			const address = APP.CORE.services.auth.accounts.eth.address;
+
+			const url = `${config.api.etherscan}?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${config.apiKeys.etherscan}`
+			let transactions
+			$.ajax({
+				type : 'GET',
+				url : url,
+				complete : function (rv) {
+					const res = rv.responseJSON;
+					if (res.status) {
+						transactions = res.result
+							.filter((item) => item.value > 0).map((item) => ({
+								type: 'eth',
+								confirmations: item.confirmations,
+								hash: item.hash,
+								status: item.blockHash != null ? 1 : 0,
+								value: APP.CORE.env.web3.utils.fromWei(item.value),
+								address: item.to,
+								date: item.timeStamp * 1000,
+								direction: address.toLowerCase() === item.to.toLowerCase() ? 'in' : 'out',
+							}));
+						
+						if (callback instanceof Function) {
+							callback(transactions);
+						};
+						resolve(transactions);
+					}
+					else {
+					  console.error('res:status ETH false', res)
+					}
+				}
+			});
+		});
+	},
+	send : function (from, to, amount, callback , callbackerror) {
+		new Promise(async (resolve, reject) => {
+			const privateKey = APP.CORE.services.auth.accounts.eth.privateKey;
+			const params = {
+				to: String(to).trim(),
+				gasPrice: '20000000000',
+				gas: '21000',
+				value: APP.CORE.env.web3.utils.toWei(String(amount)),
+			};
+			let txRaw;
+
+			await APP.CORE.env.web3.eth.accounts.signTransaction(params, privateKey)
+				.then(result => {
+					txRaw = APP.CORE.env.web3.eth.sendSignedTransaction(result.rawTransaction)
+				})
+
+			const receipt = await txRaw.on('transactionHash', (hash) => {
+				/*
+				const txId = `${config.link.etherscan}/tx/${hash}`
+				actions.loader.show(true, true, txId)
+				*/
+			})
+			.on('error', (err) => {
+				reject(err);
+				if (callbackerror instanceof Function) {
+					callbackerror(err);
+				}
+			})
+
+			resolve(receipt)
+			if (callback instanceof Function) {
+				callback(receipt);
+			}
+		})
 	}
 }
